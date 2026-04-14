8d2c0bcb4d4b85c58506d15d5a3f8bba999479c7
Bill Engels <w.j.engels@gmail.com>
2026-04-14 16:58:17 -0700

    update demo, add missing file

---
+++ ptgp/optim/__init__.py
@@ -0,0 +1,4 @@
+from ptgp.optim.optimizers import adam, sgd
+from ptgp.optim.training import compile_training_step, compile_predict, get_trained_params
+
+__all__ = ["adam", "sgd", "compile_training_step", "compile_predict", "get_trained_params"]
